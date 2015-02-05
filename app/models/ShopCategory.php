7a5096262eace91dace5b6550b7b5eac504aace9
zzlehieuzz <zzlehieuzz@gmail.com>
2015-02-05 18:25:33 +0700

    [add] new

--- app/models/ShopCategory.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class ShopCategory extends Base
-{
-    protected $table = 'shop_category';
-    public $timestamps = false;
-}
--- app/models/ShopRoad.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class ShopRoad extends Base
-{
-    protected $table = 'shop_road';
-    public $timestamps = false;
-}
