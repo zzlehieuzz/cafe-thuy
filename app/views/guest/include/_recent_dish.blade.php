0388e4133617783b4d2f112ca6ecd91869b55e38
zzlehieuzz <zzlehieuzz@gmail.com>
2015-02-13 12:58:47 +0700

    [add] new

--- app/views/guest/include/_recent_dish.blade.php
+++ app/views/guest/include/_recent_dish.blade.php
@@ -2,7 +2,7 @@
     @if (isset($dish))
         @foreach($dish as $dishItem)
             <li>
-                <h3><a>{{$dishItem['title']}}</a> <b>( {{ number_format($dishItem['price'])}} VNĐ )</b></h3>
+                <h3><a>{{$dishItem['title']}}</a> <b>( {{ number_format($dishItem['price'])}} VND )</b></h3>
                 <a href="{{ URL('home/menu') }}" class="readmore">Read more</a>
             </li>
         @endforeach
